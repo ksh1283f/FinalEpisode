ff77e3056b07659649763cd614649778ea813c01
ksh1283f <ksh1283f@naver.com>
2019-07-03 15:41:48 +0900

    190703
    190703 작업
    
    - 마을 컨텐츠 아이콘에 텍스트 띄우는 작업

--- Graduated_190330/Assets/Scripts/LobbyContents.cs
+++ Graduated_190330/Assets/Scripts/LobbyContents.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyContents : MonoBehaviour
 {
     public E_LobbyContents contentsType;
     public Action<E_LobbyContents> OnExecuteContets { get; set; }
+    [SerializeField] Text contentsNameText;
+
+    void Start()
+    {
+        if(contentsNameText == null)
+        {
+            Debug.LogError("contentsNameText is null");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        switch (contentsType)
+        {
+            case E_LobbyContents.UserInfo:
+                sb.Append("사무소");
+                sb.AppendLine();
+                sb.Append("<유저 정보>");
+                break;
+
+            case E_LobbyContents.CharacterProperty:
+                sb.Append("연구소");
+                sb.AppendLine();
+                sb.Append("<전투 특성 선택>");
+                break;
+            case E_LobbyContents.CharacterManage:
+                sb.Append("훈련소");
+                sb.AppendLine();
+                sb.Append("<용병 캐릭터 관리>");
+                break;
+
+            case E_LobbyContents.Etc:
+                sb.Append("TODO 기타 추가");
+                sb.AppendLine();
+                sb.Append("<추후 시장 작업>");
+                break;
+
+            case E_LobbyContents.ToBattle:
+                sb.Append("던전");
+                sb.AppendLine();
+                sb.Append("<용병 훈련 및 도전모드>");
+                break;
+        }
+
+        contentsNameText.text = sb.ToString();
+    }
 }
