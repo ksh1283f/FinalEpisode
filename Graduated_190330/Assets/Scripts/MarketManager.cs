8bc51b5a7aee5d3ec7e9d322a11e106d3f85dab3
ksh1283f <ksh1283f@naver.com>
2019-08-17 22:40:47 +0900

    190817
    - 마켓 작업 시작

---
+++ Graduated_190330/Assets/Scripts/MarketManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_MarketEventType
+{
+    None,
+    Discount,   // 특정 용병 할인
+    UrgentWant, // 특정 용병 급구
+
+}
+
+public class MarketManager : Singletone<MarketManager>
+{
+    /* 마켓 이벤트
+        특정 용병을 이벤트에 따라 다르게 구매 또는 판매 가능
+        몇턴동안 지속
+        턴은 전투를 시작할때 카운트
+
+        뉴스 시스템
+        - ex. 조만간 어떤 사건이 발생하여 어떤 용병이 많이 필요해질것이다.
+        - ex. 
+    */
+    void Start()
+    {
+        
+    }
+}
